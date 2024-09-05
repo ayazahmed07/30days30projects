82a3311b6989d08b08b651b3820491ceb8547661
Ayaz ahmed <ayazahmed824@gmail.com>
2024-09-05 11:31:03 +0000

    dash

---
+++ 03_birthday_wish_app/app/page.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Header from "./_components/Header"
+
+export default function Home() {
+  return (
+          <div> 
+           <Header/>
+            </div>
+            
+  );
+}
