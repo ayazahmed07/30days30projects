802537c15d9ce5df8ef419fad81f834f0e8e4187
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-09-15 18:02:28 +0500

    birtdaywishapp

---
+++ 03_brithdayapp/birthdaywishapp/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+import BirthdayWish from "@/components/birthday-wish";
+
+export default function Home() {
+  return (
+    <div>
+      <BirthdayWish />
+    </div>
+  );
+}
